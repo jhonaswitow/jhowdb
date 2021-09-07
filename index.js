84fd682418c293b3dae523167ce9c9f609452920
jhonaswitow <86085642+jhonaswitow@users.noreply.github.com>
2021-09-07 20:50:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const fs = require("fs")
+const fs = require("graceful-fs")
 
 module.exports = class Jhow {
   constructor(path) {
     this.path = path ? path : "jhowdb.json"
     if(!this.path.endsWith(".json")) this.path = path + ".json"
+if(this.path.startsWith("./")) throw new TypeError(`Renew ${path} (take off ./)`)
 
     if(!fs.existsSync("./" + this.path)) fs.writeFileSync(this.path, JSON.stringify({ }))
+		this.file = JSON.parse(fs.readFileSync(this.path, "utf8"))
 
   }
 
@@ -25,6 +27,9 @@ module.exports = class Jhow {
     )
     if(!option) throw new TypeError(
       "No option has been defined! If you have any questions, please go to: https://discord.gg/zTXBgXFsAY"
+    )
+    if(typeof option === 'object') throw new TypeError(
+      ""
     )
     let file = JSON.parse(fs.readFileSync(this.path, "utf8"))
 
@@ -116,4 +121,27 @@ module.exports = class Jhow {
 
    return file
  }
+
+push(key, option) {
+	if(!key) throw new TypeError("You didn't set a key!")
+	if(!option) throw new TypeError(
+		"You didn't set an option!"
+	)
+	key = String(key).split('/')
+  let b = this.file
+  while (key.length > 1) {
+    const a = key.shift()
+    b = b[a] = b[a] || {}
+  }
+
+	let value = b[key]
+	if(!value) return null
+	if(!Array.isArray(value)) value = [];
+	option = Array.isArray(option) ? option : [option]
+
+	value.push(...option)
+	b[key] = value
+ fs.writeFileSync(this.path, JSON.stringify(this.file, null, 2))
+ return value
+  }
 }
